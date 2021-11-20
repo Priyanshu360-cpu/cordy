fdd301ba43100473a5cc6846641bb85bbfd4ba6e
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2021-11-21 02:35:24 +0530

    Updated

--- src/afk.js
+++ src/afk.js
@@ -1,8 +1,11 @@
-module.exports = async (message, args, client,db) =>{
+module.exports = async (message, args, client) =>{
     const {Collection, MessageEmbed} = require('discord.js')
-    if(!db) return;
-    let a = db.get(`AFK_${message.guild.id}#${message.author.id}`)
-    if(!a)db.set(`AFK_${message.guild.id}#${message.author.id}`,"true")
+    try{
+    if(!client.afk) client.afk = new Collection()
+    setTimeout(()=>testing(),500)
+    function testing(){
+    let a = client.afk.get(`AFK_${message.guild.id}#${message.author.id}`)
+    if(!a)client.afk.set(`AFK_${message.guild.id}#${message.author.id}`,"true")
     else{
         if(a == "true"){
             let a = new MessageEmbed()
@@ -15,32 +18,31 @@ module.exports = async (message, args, client,db) =>{
             a.setDescription("You are afk now")
             a.setColor("RED")
             message.channel.send({embeds:[a]})
-            db.set(`AFK_${message.guild.id}#${message.author.id}`,"true")
-            db.set(`AFKT_${message.guild.id}#${message.author.id}`,`${Date.now()}`)
+            client.afk.set(`AFK_${message.guild.id}#${message.author.id}`,"true")
+            client.afk.set(`AFKT_${message.guild.id}#${message.author.id}`,`${Date.now()}`)
         }
     }
     }
-    if(db.get("main")!="true"){
-            db.set("main","true")
-    client.on('messageCreate', async (message,db) => {
-        if(!db) return;
-        let b = db.get(`AFK_${message.guild.id}#${message.author.id}`)
+    if(client.afk.get("main")!="true"){
+            client.afk.set("main","true")
+    client.on('messageCreate', async (message) => {
+        let b = client.afk.get(`AFK_${message.guild.id}#${message.author.id}`)
         if(b == "true"){
             let c = new MessageEmbed()
             c.setTitle('Welcome Back')
             c.setDescription(`We removed Your Afk <@${message.author.id}>`)
             c.setColor("FFCBCB")
             let w = await message.reply({embeds:[c]})
-            db.set(`AFK_${message.guild.id}#${message.author.id}`,"false")
+            client.afk.set(`AFK_${message.guild.id}#${message.author.id}`,"false")
             setTimeout(()=>w.delete(),5000)
         }
         if(!message.mentions.users.first()) return;
         else{
-        let d = db.get(`AFK_${message.guild.id}#${message.mentions.users.first().id}`)
+        let d = client.afk.get(`AFK_${message.guild.id}#${message.mentions.users.first().id}`)
         if(d == "true"){
             let e = new MessageEmbed()
             e.setTitle('Afk User')
-            e.setDescription(`User afk since <t:${db.get(`AFKT_${message.guild.id}#${message.mentions.users.first().id}:F>`)}`)
+            e.setDescription(`User afk since <t:${client.afk.get(`AFKT_${message.guild.id}#${message.mentions.users.first().id}`)}:F>`)
             e.setFooter("npm i cordy")
             e.setColor("FFCBCB")
             message.reply({embeds:[e]})
@@ -48,3 +50,9 @@ module.exports = async (message, args, client,db) =>{
     }
     })
     }
+    }
+    catch(err){
+        console.log("Error Caught")
+    }
+    }
+    
