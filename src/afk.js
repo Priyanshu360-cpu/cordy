286f9eed6d325f0df3f48620b0d5e1b4e6b77096
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2021-11-21 02:49:48 +0530

    Updated Final

--- src/afk.js
+++ src/afk.js
@@ -1,4 +1,5 @@
-module.exports = async (message, args, client) =>{
+module.exports = async (message, args, client,db) =>{
+    
     const {Collection, MessageEmbed} = require('discord.js')
     try{
     if(!client.afk) client.afk = new Collection()
@@ -15,17 +16,20 @@ module.exports = async (message, args, client) =>{
         }
         else{
             let a = new MessageEmbed()
-            a.setDescription("You are afk now")
+            a.setDescription(`You are afk now <@${message.author.id}>`)
             a.setColor("RED")
             message.channel.send({embeds:[a]})
             client.afk.set(`AFK_${message.guild.id}#${message.author.id}`,"true")
             client.afk.set(`AFKT_${message.guild.id}#${message.author.id}`,`${Date.now()}`)
+            client.afk.set(`AFKM_${message.guild.id}#${message.author.id}`,`${args}`)
         }
     }
     }
     if(client.afk.get("main")!="true"){
             client.afk.set("main","true")
     client.on('messageCreate', async (message) => {
+        if (message.author.bot) return;
+        if (!message.guild) return;
         let b = client.afk.get(`AFK_${message.guild.id}#${message.author.id}`)
         if(b == "true"){
             let c = new MessageEmbed()
@@ -42,7 +46,7 @@ module.exports = async (message, args, client) =>{
         if(d == "true"){
             let e = new MessageEmbed()
             e.setTitle('Afk User')
-            e.setDescription(`User afk since <t:${client.afk.get(`AFKT_${message.guild.id}#${message.mentions.users.first().id}`)}:F>`)
+            e.setDescription(`User afk since <t:${client.afk.get(`AFKT_${message.guild.id}#${message.mentions.users.first().id}`)}:F>\nMessage-${client.afk.get(`AFKM_${message.guild.id}#${message.mentions.users.first().id}`)}`)
             e.setFooter("npm i cordy")
             e.setColor("FFCBCB")
             message.reply({embeds:[e]})
