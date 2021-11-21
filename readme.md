8a003f86a26d17d8bc81239ef5b8bc6ddeb4ff80
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2021-11-21 18:06:00 +0530

    Updated

--- readme.md
+++ readme.md
@@ -3,11 +3,51 @@ Install Cordy
  ```
 npm i cordy
 ```
-Cordy Version - 1.0.2
-# How To Use
+Cordy Version - 1.0.3
+# what is Cordy?
+Cordy is an open soruce npm package which eases your discord bot making
+# To Start your bot easily 
 ```
-const game = require("cordy")
-game.emoji(message,args,client)
+const cordy = require('cordy')
+cordy.starter("token",{
+    status:"Cordy Goes Broom , Testing",
+    statusType:"PLAYING",
+    statusVisibility:"DND"
+})
 ```
-It is Still Being Developed
-Visit https://github.com/Priyanshu360-cpu/cordy to view the commands
+<img src = "https://media.discordapp.net/attachments/750089917501014036/911954534862049280/unknown.png"></img>
+
+# How to use Cordy in your Bot?
+read readme folders to get detailed usage of each command
+heres Some example: 
+
+**Chatbot**
+```
+const cordy = require('cordy')
+cordy.chatbot(message,args,client,{
+    color:"FFCBCB"
+    owner:"Priyanshu"
+})
+```
+<img src = "https://media.discordapp.net/attachments/750089917501014036/911955270530379796/unknown.png"></img>
+
+**Automeme**
+```
+const cordy = require('cordy')
+ cordy.automeme(message,args,client,{
+ timer:args[0]})
+ ```
+<img src = "https://media.discordapp.net/attachments/750089917501014036/911955587993042954/unknown.png"></img>
+
+**FindTheEmoji**
+```
+const cordy = require('cordy')
+cordy.emoji(message,args,client)
+```
+<img src = "https://media.discordapp.net/attachments/750089917501014036/911956598669316136/unknown.png"></img>
+
+**Make sure this codes are in you message create event**
+
+*Made with ❤️ by Priyanshu*
+
+
---
+++ src/spotify.js
@@ -0,0 +1,60 @@
+module.exports = async(message,args,client) => {
+    const {MessageEmbed} = require("discord.js");
+    const axios = require("axios")
+    switch(args[0].includes("spotify.com/track")){
+      case true:
+        let p = args[0].split('track/')[1];
+        let c = p.split('?')[0];
+        let d = 'https://embed.spotify.com/oembed/?url=spotify:track:'+c
+        axios.get(d)
+        .then((response) => {
+        const cu = response.data.thumbnail_url;
+        var thing = new MessageEmbed()
+                        .setColor(client.embedColor)
+                                .setTimestamp()
+                                .setTitle(`${response.data.title}`)
+                        .setImage(cu)
+        return message.channel.send({embeds: [thing]})})
+        break;
+      case false:
+          switch(args[0].includes("spotify.com/playlist")){
+              case true:
+                let p = args[0].split('playlist/')[1];
+                let c = p.split('?')[0];
+                let d = 'https://embed.spotify.com/oembed/?url=spotify:playlist:'+c
+                axios.get(d)
+                .then((response) => {
+                const cu = response.data.thumbnail_url;
+                var thing = new MessageEmbed()
+                                .setColor(client.embedColor)
+                                .setTimestamp()
+                                .setTitle(`${response.data.title}`)
+                                .setImage(cu)
+                return message.channel.send({embeds: [thing]})})
+                break;
+             case false:
+                 switch(args[0].includes("spotify.com/album")){
+                     case true:
+                        let p = args[0].split('album/')[1];
+                        let c = p.split('?')[0];
+                        let d = 'https://embed.spotify.com/oembed/?url=spotify:album:'+c
+                        
+                        axios.get(d)
+                        .then((response) => {
+                        const cu = response.data.thumbnail_url;
+                        var thing = new MessageEmbed()
+                                        .setColor(client.embedColor)
+                                        .setTitle(`${response.data.title}`)
+                                        .setImage(cu)
+                                        .setTimestamp()
+                        return message.channel.send({embeds: [thing]})})
+                        break;
+                     case false:
+                         return message.reply("Not a spotify Url")
+
+                 }
+
+          }
+
+                        
+}}
