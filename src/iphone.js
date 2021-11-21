2115e35c0bf89b337869a7b801b0d426d46d50a6
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2021-11-21 13:44:50 +0530

    Added

---
+++ src/iphone.js
@@ -0,0 +1,14 @@
+module.exports = async (message, args, client) => {
+    const axios = require('axios');
+      const url = 'https://shot-on-iphone.studio/api/video';
+      let response, data;
+      try {
+          response = await axios.get(url);
+          data = response.data;
+      } catch (e) {
+          return message.reply(`An error occured!`)
+      }
+
+
+      await message.reply({content : `${data.url}`})
+    }
